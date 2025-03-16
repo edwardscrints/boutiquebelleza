e4a289aa7e20972727601f689313b1ef2d4ec5c3
origin <edwardscrints@gmail.com>
2025-03-15 19:53:56 -0500

    Ajuste de diseño view Welcome-banner

--- resources/views/components/welcome-banner.blade.php
+++ resources/views/components/welcome-banner.blade.php
@@ -1,28 +1,30 @@
-<div class="slider relative overflow-hidden max-h-96 w-full">
-    @foreach ($this->getSlidersProperty() as $index => $slider)
-        <img src="{{ asset($slider['image']) }}" alt="{{ $slider['name'] }}" 
-             class="slide w-full h-full object-cover transition-opacity duration-500 ease-in-out 
-             {{ $index == 0 ? 'opacity-100' : 'opacity-0' }}">
-    @endforeach
-    <div class="max-w-xl py-32 mx-auto text-center">
-        <h1 class="text-3xl font-extrabold sm:text-5xl">
-            Tu Belleza a Domicilio.
-            <span class="text-indigo-600">Boutique de Belleza</span>
-            <span role="img" aria-hidden="true">👋</span>
-        </h1>
-    </div>
-</div>
-<section class="bg-gray-50 section-below-slider z-50">       
-        <div class="grid grid-cols-6 gap-2 mt-8">
-        @foreach ($this->getSubcaProperty() as $index => $subcat)
-            <figure class>
-                <img src="{{ asset($subcat['image']) }}" alt="{{ $subcat['name'] }}" class="w-full h-full">
-            </figure>
-        @endforeach        
-        
+
+    <div class="slider fixed overflow-hidden max-h-96 w-full rounded-lg">
+        @foreach ($this->getSlidersProperty() as $index => $slider)
+            <img src="{{ asset($slider['image']) }}" alt="{{ $slider['name'] }}" 
+                class="slide w-full h-full object-cover transition-opacity duration-500 ease-in-out 
+                {{ $index == 0 ? 'opacity-100' : 'opacity-0' }}">
+        @endforeach
+        <div class="max-w-xl py-32 mx-auto text-center">
+            <h1 class="text-3xl font-extrabold sm:text-5xl">
+                Tu Belleza a Domicilio.
+                <span class="text-indigo-600">Boutique de Belleza</span>
+                <span role="img" aria-hidden="true">👋</span>
+            </h1>
         </div>
-        
-</section>
+    </div>
+    <section>
+        <div class="bg-gray-50 section-below-slider overflow-hidden">       
+            <div class="grid grid-cols-6 gap-2 mt-8 sm:relative border-4 bg-white">
+            @foreach ($this->getSubcaProperty() as $index => $subcat)
+                <figure class>
+                    <img src="{{ asset($subcat['image']) }}" alt="{{ $subcat['name'] }}" class="lg:h-73 w-full h-full object-cover">
+                </figure>
+            @endforeach        
+            </div>
+        </div>
+    </section>        
+
 
 {{--Botones por agregar
     <div class="flex flex-wrap justify-center gap-4 mt-8">
