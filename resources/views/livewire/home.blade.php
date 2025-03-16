6b8d67ef9a97a1501a33e2193e1d93726a4cba02
origin <edwardscrints@gmail.com>
2025-03-15 20:08:02 -0500

    Ajuste a diseño de Secciones sale y fotter de home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,7 +2,7 @@
     <x-welcome-banner />
     <div class="slider">
     </div>
-    <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-12 sm:px-6 lg:px-8">
+    <div class="px-4 py-12 mx-auto space-y-12 sm:px-6 lg:px-8">
         @if ($this->saleCollection)
             <x-collection-sale />
         @endif
